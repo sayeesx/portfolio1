17b3160aca5e24fe029e79fcc641fd12064dd98c
sayeesck <151899876+sayeesck@users.noreply.github.com>
2025-05-21 17:11:36 +0530

    api connected

--- components/chatWidget.jsx
+++ components/chatWidget.jsx
@@ -89,7 +89,7 @@ export default function ChatWidget() {
     await handleTypingAnimation();
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/chatbot", {
+      const res = await fetch("https://chatbot-3-ayjr.onrender.com/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message }),
